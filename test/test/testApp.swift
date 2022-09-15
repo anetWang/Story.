3883b229ea7b598f3a34f776e0ffe54112150313
APPLE <apple@APPLEtekiMacBook-Air-4.local>
2022-09-15 15:40:49 +0800

    Initial Commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by APPLE on 2022/9/15.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
